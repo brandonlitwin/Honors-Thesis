f25b922013dde27fa8061c076fbc90a868badae7
Brandon Litwin <brandon.litwin1@marist.edu>
2019-04-23 11:12:47 -0400

    Changed to comparing abs value of sql values, and printing a song name

--- comparesongs.py
+++ comparesongs.py
@@ -14,7 +14,7 @@ song_list = os.listdir("TestSongs")
 num_songs = len(song_list)
 song_data = np.zeros(shape=(num_songs,songlen))
 # Get the samples from each song
-fname = "Cold.wav"
+fname = "Battery.wav"
 song = AudioSegment.from_wav("TestSongs/"+fname)
 samples = np.array(song.get_array_of_samples())
 samples = samples[:songlen]
@@ -59,20 +59,25 @@ mydb = mysql.connector.connect(
     database="honors"
 )
 mycursor = mydb.cursor()
-
+sql = "SELECT id, name FROM songs WHERE id = (SELECT id FROM song_data ORDER BY ABS (xdata - " + str(encoded_song[0][0]) + ") + ABS (ydata - " + str(encoded_song[0][1]) + ") LIMIT 1)"
+print(sql)
+mycursor.execute(sql)
+result = mycursor.fetchone()
+print(result[0], result[1])
 mycursor.execute("SELECT xdata, ydata FROM song_data")
 result = mycursor.fetchall()
 
 xdata = []
 ydata = []
 for val in result:
+    print(val[0],val[1])
     xdata.append(val[0])
     ydata.append(val[1])
    
-closest_song = (min(xdata, key=lambda x:abs(x-encoded_song[0][0])), min(ydata, key=lambda x:abs(x-encoded_song[0][1])))
+"""closest_song = (min(xdata, key=lambda x:abs(x-encoded_song[0][0])), min(ydata, key=lambda x:abs(x-encoded_song[0][1])))
 print(closest_song)
 sql = "SELECT name from songs INNER JOIN song_data ON songs.id = song_data.id WHERE xdata = " + str(closest_song[0]) + " AND ydata = " + str(closest_song[1])
 print(sql)
 mycursor.execute(sql)
 result = mycursor.fetchone()
-print(result[0])
+print(result[0])"""
