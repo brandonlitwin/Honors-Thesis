182284baa275db649b8746d7fefb1c67cb1b64f2
Brandon Litwin <brandon.litwin1@marist.edu>
2019-04-25 11:44:08 -0400

    Started upload page

--- comparesongs.py
+++ comparesongs.py
@@ -10,32 +10,31 @@ import mysql.connector
 
 songlen = 2500000
 # Create an empty np array of size n where n is the total num of songs
-song_list = os.listdir("TestSongs")
-num_songs = len(song_list)
-song_data = np.zeros(shape=(num_songs,songlen))
+#song_list = os.listdir("TestSongs")
+#num_songs = len(song_list)
+#song_data = np.zeros(shape=(num_songs,songlen))
 # Get the samples from each song
-fname = "Battery.wav"
-song = AudioSegment.from_wav("TestSongs/"+fname)
+#fname = "Wokeuplikethis.wav"
+import glob
+list_of_files = glob.glob('Uploads/*.wav')
+fname = max(list_of_files, key=os.path.getctime)
+#print(fname)
+song = AudioSegment.from_wav(fname)
 samples = np.array(song.get_array_of_samples())
 samples = samples[:songlen]
 song_data = np.array(samples)
 encoding_dim = 2
 
 input_song = Input(shape=(songlen,))
-print(input_song.shape)
-#encoded = Dense(32, activation='linear')(input_song)
-#encoded = Dense(8, activation='linear')(encoded)
-#encoded = Dense(encoding_dim, activation='linear')(input_song)
-#print(encoded)
-#decoded = Dense(8, activation='linear')(encoded)
-#decoded = Dense(32, activation='linear')(decoded)
-#decoded = Dense(songlen, activation='sigmoid')(encoded)
-# Map input to its reconstruction
-import glob
-list_of_files = glob.glob('model*.h5')
-filename = max(list_of_files, key=os.path.getctime)
-autoencoder = load_model(filename)
+#print(input_song.shape)
 
+# Map input to its reconstruction
+"""import glob
+list_of_files = glob.glob('model*.h5')
+filename = max(list_of_files, key=os.path.getctime)"""
+filename = 'model-ep31.h5'
+autoencoder = load_model(filename)
+#print(filename)
 x_test = song_data.reshape(1,-1)
 
 x_test = x_test.astype('float32')
@@ -45,11 +44,11 @@ scaler = joblib.load('songScaler.pkl')
 x_test = scaler.transform(x_test)
 
 encoded_song = autoencoder.predict(x_test)
-print(encoded_song)
+#print(encoded_song)
 
 scaler = joblib.load('displayScaler.pkl') 
 encoded_song = scaler.transform(encoded_song)
-print(encoded_song)
+#print(encoded_song)
 
 # Time to compare the new song to the database
 
@@ -59,20 +58,22 @@ mydb = mysql.connector.connect(
     database="honors"
 )
 mycursor = mydb.cursor()
-sql = "SELECT id, name FROM songs WHERE id = (SELECT id FROM song_data ORDER BY ABS (xdata - " + str(encoded_song[0][0]) + ") + ABS (ydata - " + str(encoded_song[0][1]) + ") LIMIT 1)"
-print(sql)
+sql = "SELECT name FROM songs WHERE id = (SELECT id FROM song_data ORDER BY ABS (xdata - " + str(encoded_song[0][0]) + ") + ABS (ydata - " + str(encoded_song[0][1]) + ") LIMIT 1)"
+#print(sql)
 mycursor.execute(sql)
 result = mycursor.fetchone()
-print(result[0], result[1])
-mycursor.execute("SELECT xdata, ydata FROM song_data")
+data = result[0]
+print(data)
+sys.stdout.flush()
+"""mycursor.execute("SELECT xdata, ydata FROM song_data")
 result = mycursor.fetchall()
 
 xdata = []
 ydata = []
 for val in result:
-    print(val[0],val[1])
+    #print(val[0],val[1])
     xdata.append(val[0])
-    ydata.append(val[1])
+    ydata.append(val[1])"""
    
 """closest_song = (min(xdata, key=lambda x:abs(x-encoded_song[0][0])), min(ydata, key=lambda x:abs(x-encoded_song[0][1])))
 print(closest_song)
---
+++ upload.js
@@ -0,0 +1,53 @@
+//const ps = require('python-shell');
+var util = require("util");
+const spawn = require("child_process").spawn;
+const pyProcess = spawn('python', ['comparesongs.py']);
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.path;
+      var newpath = '/mnt/c/Users/Brandon/gitprojects/Honors-Thesis/Uploads/' + files.filetoupload.name;
+      fs.copyFile(oldpath, newpath, function (err) {
+        if (err) throw err;
+        //res.write('File uploaded and moved! ');
+	//res.write('Generating Song Recommendation... ');
+	util.log('before python called');
+	pyProcess.stdout.on('data', function(data) {
+	  console.log(data.toString());
+	  util.log(data.toString());
+	  res.send(data.toString());
+	  res.write(data.toString());
+	});
+	/*ps.run('comparesongs.py', null, function (err, results) {
+  	  if (err) throw err;
+  	  console.log('results: %j', results);
+	  res.write('hey it worked');
+	  res.write(results);
+	});*/
+	res.end();
+      });
+ });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Song Recommender</h1>');
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    util.log('starting script');
+    pyProcess.stdout.on('data', function(data) {
+      console.log(data);
+      util.log(data);
+      util.log(data.toString());
+      res.send(data.toString());
+      res.write(data.toString());
+    });
+    util.log('finished script');
+    return res.end();
+  }
+}).listen(8080);
