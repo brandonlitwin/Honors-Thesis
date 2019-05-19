Fixed scaling

ae-normal.py
@@ -42,7 +42,7 @@ x_test = song_data
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 #print(x_train.dtype)
-scaler = MinMaxScaler()
+scaler = MinMaxScaler(feature_range=(0,1))
 scaler.fit(x_train)
 x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
@@ -69,12 +69,13 @@ np.save(str(num_songs)+'_normal_encoded_songs_'+sys.argv[1]+'_epochs',encoded_so
 
 
 import matplotlib.pyplot as plt
-
+encoded_songs = scaler.fit_transform(encoded_songs)
+print(encoded_songs)
 count = 0
 plt.figure()
 for fname in os.listdir("TestSongs/"):
   plt.scatter(encoded_songs[count][0], encoded_songs[count][1], s=700,
               c=(int(encoded_songs[count][1]/10.0),0,int(1-encoded_songs[count][1]/10.0)),
-              marker=r"$ {} $".format(fname[:2]), edgecolors='none')
+              marker=r"$ {} $".format(fname[:4]), edgecolors='none')
   count += 1
 plt.show()